3ee0ef9144f0c6fd40ef45c3417242d73fe47b6c
jose <bustamanteg@gmail.com>
2024-03-24 19:30:41 -0500

    added explanation of the channels

--- nc_to_gwy_stable.py
+++ nc_to_gwy_stable.py
@@ -2,6 +2,37 @@
 # -*- coding: utf-8 -*-
 
 # Code that converts scans in the nc format, produced by gxsm to gwy format. Each channel is saved in the same file. 
+'''
+This code creates the following channels: 
+
+
+1 Topography (right)
+2 Topography (left)
+3. Frequency shift (right)
+4. Frequency shift (left)
+5. Dissipation (right) This dissipation is calculated assuming the mean values of the scan represent E_0=\pi k A^2 /Q
+6. Dissipation (left)  This dissipation is calculated assuming the mean values of the scan represent E_0=\pi k A^2 /Q
+7. Amplitude (right)  Oscillation amplitude nm
+8. Amplitude (left )  Oscillation amplitude nm
+9. 1 \omega amplitude  (right) If the voltage applied is modulated, this is the amplitude of the first sideband
+10. 1 \omega amplitude (left)
+11. PID Phase right 
+12. PID Phase (left)
+13. Excitation amplitude (right) [V]            Voltage Amplitude aplied to the excitation system to mantain a constant oscillation of the cantilever. 
+14. Excitation amplitude (left)  [V]
+15. Excess excitation (right)  [Hz] or [eV]     With respect to a parameter Diss0 which is the dissipation of the cantilever only, (far away from the sample)
+16. Excess excitation (left)    [Hz] of [eV]    With respect to a parameter Diss0 which is the dissipation of the cantilever only, (far away from the sample)
+
+Channel 15 and 16 are calculated with 
+
+Diss_r0 = (Diss_r/Diss0-1)*E0_eV
+Diss_l0 = (Diss_l/Diss0-1)*E0_eV
+
+E0_J=np.pi*A**2*k/Q
+E0_eV=E0_J/1.602e-19
+
+'''
+
 # The dissipation channel can be saved in units of eV/cycle or Hz. 
 # Change the user parameters to suit your needs. 
 
@@ -150,7 +181,7 @@ def run(direct,prefix, cantsens, signalgain, Q, f0, ex_units="eV"):
         # Dissipation units in eV 
         Ecuation 2.27, pg 26 in 
         S. Morita, R. Wiesendanger, and E. Meyer, eds., Noncontact Atomic Force Microscopy.
-Springer, hardcover ed., 7 2002.
+        Springer, hardcover ed., 7 2002.
 
         L. Fairgireve-Park thesis eq. 2.7, 2.8
         '''
